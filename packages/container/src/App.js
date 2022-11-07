e5e1a92572d2314a1c958456ac8e71dc1e1273dd
Bohai Liu <liubohai922@hotmail.com>
2022-11-07 17:08:25 +0800

    Inserted header into container app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 // marketing is ModuleFederationPlugin's remote key
 // MarketingApp is exposed from marketing's ModuleFederationPlugin
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!!!!!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
