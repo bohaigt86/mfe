7e8619af6db0d19d7dbd845170733568041b9f92
Bohai Liu <liubohai922@hotmail.com>
2022-11-08 14:40:00 +0800

    fix historyapifallback issues

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,30 +1,28 @@
-const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-const packageJson = require("../package.json");
+const packageJson = require('../package.json');
 
-const commonConfig = require("./webpack.common");
+const commonConfig = require('./webpack.common');
 
 const devConfig = {
-  mode: "development",
+  mode: 'development',
   devServer: {
     port: 8081,
-    historyApiFallback: {
-      index: "index.html",
-    },
+    historyApiFallback: true,
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: "marketing",
-      filename: "remoteEntry.js",
+      name: 'marketing',
+      filename: 'remoteEntry.js',
       exposes: {
-        "./MarketingApp": "./src/bootstrap",
+        './MarketingApp': './src/bootstrap',
       },
       shared: packageJson.dependencies,
     }),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
   ],
 };
